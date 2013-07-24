8279b574da29b5db204952d6f0fba3af13ea3092
balu <balu@agiliq.com>
2013-07-25 02:11:11 +0530

    added mock pie chart

--- graphos/renderers/matplotlib_renderer.py
+++ graphos/renderers/matplotlib_renderer.py
@@ -47,3 +47,25 @@ class BarChart(BaseMatplotlibChart):
         plt.savefig(out)
         out.seek(0)
         return "data:image/png;base64,%s" % base64.encodestring(out.read())
+
+
+class PieChart(BaseMatplotlibChart):
+
+    def get_image(self):
+        import matplotlib.pyplot as plt
+        plt.figure(1, figsize=(6, 6))
+        ax = plt.axes([0.1, 0.1, 0.8, 0.8])
+        labels = self.header
+        explode = (0, 0.05, 0, 0, 0)
+        ax.pie(self.get_data()[1:][1],
+               explode=explode,
+               labels=labels,
+               autopct='%1.1f%%',
+               shadow=True)
+        plt.title(self.get_options()['title'],
+                  bbox={'facecolor': '0.8', 'pad': 5})
+
+        out = StringIO.StringIO()
+        plt.savefig(out)
+        out.seek(0)
+        return "data:image/png;base64,%s" % base64.encodestring(out.read())
