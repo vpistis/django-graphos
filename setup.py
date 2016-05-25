0b27d6b468c7a4716e99f2ff71d9bd8afe6ee7c8
valentino pistis <valentino.pistis@green-share.it>
2016-05-25 10:36:33 +0200

    compatibility python 3

--- setup.py
+++ setup.py
@@ -80,9 +80,10 @@ def find_package_data(
                         or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print >> sys.stderr, (
-                                "Directory %s ignored by pattern %s"
-                                % (fn, pattern))
+                            print("Directory %s ignored by pattern %s" % (fn, pattern), file=sys.stderr)
+                            # print >> sys.stderr, (
+ #                                "Directory %s ignored by pattern %s"
+ #                                % (fn, pattern))
                         break
                 if bad_name:
                     continue
@@ -103,9 +104,10 @@ def find_package_data(
                         or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print >> sys.stderr, (
-                                "File %s ignored by pattern %s"
-                                % (fn, pattern))
+                            print("File %s ignored by pattern %s" % (fn, pattern), file=sys.stderr)
+                            # print >> sys.stderr, (
+ #                                "File %s ignored by pattern %s"
+ #                                % (fn, pattern))
                         break
                 if bad_name:
                     continue
